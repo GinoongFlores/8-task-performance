650b9dd390d543679a3f19a6d842520caba37a1e
ginoongflores <flores.135577@cdo.sti.edu.ph>
2022-01-12 11:03:27 +0800

    expanded RunGame

--- RunGame.java
+++ RunGame.java
@@ -1,17 +1,18 @@
 package Eight_Task_Performance;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class RunGame implements Story, Survival, Name {
     public void readStory() {
-        System.out.println("1 - Story");
+        System.out.println("\nWelcome to Story Mode");
     }
     public void survivalMode() {
-        System.out.println("2 - Survival");
+        System.out.println("\nWelcome to Survival Mode");
     }
 
     public void userName(String name) {
-        System.out.print("Press P to start Playing, " + name);
+        System.out.print("Press P to start Playing, " + name + ": ");
     }
 
     public static void main(String[] args) {
@@ -20,10 +21,23 @@ public class RunGame implements Story, Survival, Name {
         System.out.print("Enter your name: ");
 
         String name = sc.nextLine();
-        System.out.println("Press 1 or 2 to select your game mode.");
-        runGame.readStory();
-        runGame.survivalMode();
-        runGame.userName(name);
+        System.out.println("Press 1 or 2 to select your game mode.\n1 - Story\n2 - Survival");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            runGame.userName(name);
+            String press = sc.next();
+            if(press.equalsIgnoreCase("P")) {
+                runGame.readStory();
+            }
+        } else if (choice == 2) {
+            runGame.userName(name);
+            String press = sc.next();
+            if (press.equalsIgnoreCase("P")) {
+                runGame.survivalMode();
+            }
+        } else {
+            System.out.println("Invalid input please try again...");
+        }
 
     }
 
